e51e3482a4ee9361e0fd7ffec02babfec9c1a8eb
Robert Adam <dev@robert-adam.de>
2023-02-21 18:18:44 +0100

    Use Numpy's own compare function

--- tests/test_spin_hamiltonians.py
+++ tests/test_spin_hamiltonians.py
@@ -6,28 +6,20 @@ from koehnlab.spin_hamiltonians.spin_utils import diagonalizeSpinHamiltonian
 # from typing import List, Sequence, Tuple
 
 import numpy as np
+from numpy.testing import assert_array_almost_equal
 
 from koehnlab.spin_hamiltonians import spinMat, tprod, diagonalizeSpinHamiltonian
 
 
 class TestFiniteDifference(unittest.TestCase):
 
-    # compare to numpy arrays to a given precision:
-    def assertNumpyArrayAlmostEqual(
-        self, first, second, places=7, msg=None, delta=None
-    ):
-        self.assertEqual(first.shape, second.shape, msg=msg)
-
-        for x1, x2 in zip(np.nditer(first), np.nditer(second)):
-            self.assertAlmostEqual(x1, x2, places=places, msg=msg, delta=delta)
-
     def test_spinMat(self):
         isq2 = np.sqrt(0.5)
-        self.assertNumpyArrayAlmostEqual(
+        assert_array_almost_equal(
             spinMat(1, "x"),
             np.array([[0.0, isq2, 0.0], [isq2, 0.0, isq2], [0.0, isq2, 0.0]]),
         )
-        self.assertNumpyArrayAlmostEqual(
+        assert_array_almost_equal(
             spinMat(1, "y"),
             np.array(
                 [
@@ -39,7 +31,7 @@ class TestFiniteDifference(unittest.TestCase):
         )
         sq2j = np.sqrt(2.0) * 1j
         sq5hj = np.sqrt(5.0) / 2.0 * 1j
-        self.assertNumpyArrayAlmostEqual(
+        assert_array_almost_equal(
             spinMat(5 / 2, "y"),
             np.array(
                 [
@@ -52,15 +44,15 @@ class TestFiniteDifference(unittest.TestCase):
                 ]
             ),
         )
-        self.assertNumpyArrayAlmostEqual(
+        assert_array_almost_equal(
             spinMat(5 / 2, "z"), np.diag([5 / 2, 3 / 2, 1 / 2, -1 / 2, -3 / 2, -5 / 2])
         )
-        self.assertNumpyArrayAlmostEqual(spinMat(3 / 2, "X"), spinMat(3 / 2, "x"))
+        assert_array_almost_equal(spinMat(3 / 2, "X"), spinMat(3 / 2, "x"))
 
     def test_tprod(self):
         A = np.array([[0.0, -1.0], [1.0, 2.0]])
         B = B = np.diag([1.0, 2.0, 3.0])
-        self.assertNumpyArrayAlmostEqual(
+        assert_array_almost_equal(
             tprod(A, B),
             np.array(
                 [
@@ -110,7 +102,7 @@ class TestFiniteDifference(unittest.TestCase):
         MmatZ = np.diag([3.0, 1.0, -1.0, -3.0])
 
         ev1, U = diagonalizeSpinHamiltonian(Hmat)
-        self.assertNumpyArrayAlmostEqual(
+        assert_array_almost_equal(
             ev1, np.array([-248.85528726, -248.85528726, -26.14471274, -26.14471274])
         )
         # test also standard choice of Kramers pair:
@@ -125,7 +117,7 @@ class TestFiniteDifference(unittest.TestCase):
         ev2, _ = diagonalizeSpinHamiltonian(
             Hmat, np.array([MmatX, MmatY, MmatZ]), np.array([0.1, 0.1, 5.0])
         )
-        self.assertNumpyArrayAlmostEqual(
+        assert_array_almost_equal(
             ev2, np.array([-255.80378613, -241.91164761, -28.42353524, -23.86103102])
         )
 
