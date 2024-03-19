Fixing missing functions and deleting unnessecary function. Changing in description with units of input and output. Also changing some variable names for better understanding. Changing the datatype of molpro export function input

packages/koehnlab/io/basis_util.py
@@ -1,6 +1,61 @@
 import numpy as np
+from spin_hamiltonians import spin_utils
 
-def get_multispinmat_prod(s,spat_nums,coord):
+def get_propmat_prod(l,multiplicity:int,spat_num:int):
+        """
+        Returns the given l matrix in productbasis (spin-spatial basis).
+        Args:
+        ---------------------
+        l -- matrix of angular momentum in (kg*m^2)/s
+        multiplicity -- multiplicity of the system
+        spat_num - number of spatial states
+        Returns:
+        ---------------------
+        Lmat -- Matrix of angular momentum in productbasis in (kg*m^2)/s
+        """
+        dim = multiplicity*spat_num
+        Lmat = np.zeros((dim,dim),dtype = complex)
+        for x in range(dim):
+                for y in range(dim):
+                        ms = m.floor(x/spat_num)
+                        ms_strich = m.floor(y/spat_num)
+                        i = x%spat_num
+                        j = y%spat_num
+                        if ms == ms_strich:
+                            Lmat[x,y] = l[i,j]
+        return Lmat
+
+def get_spinmat_prod(s,multiplicity:int,spat_num:int)
+        """
+        Computes the spin matrix elements of the given spin matrix in the productbasis (spin-spatial basis)
+        Blocked over ms number.
+        Args:
+        ------------------------
+        s -- spin matrix in spin basis in Js
+        multiplicity -- Multiplicity of the system
+        spat_num -- number of spatial states
+
+        Returns:
+        ------------------------
+        SmatX,SmatY,SmatZ -- spin matrix in the productbasis in Js
+        """
+        dim = multiplicity*spat_num
+        spin = 0.5*(multiplicity-1)
+        Smat = np.zeros((dim,dim),dtype = complex)
+        if spin == 0:
+                return Smat
+        else:
+                for x in range(dim):
+                        for y in range(dim):
+                                ms = m.floor(x/spat_num)
+                                ms_strich = m.floor(y/spat_num)
+                                i = x%spat_num
+                                j = y%spat_num
+                                if i==j:
+                                        Smat[x,y] = s[ms,ms_strich]
+                return Smat
+
+def get_multispinmat_prod(spins,spat_nums,coord:str):
         """
         Returns the spin matrix of all given Spins S in the productbasis of all spins and all spatial coordinates
 
@@ -8,61 +63,56 @@ def get_multispinmat_prod(s,spat_nums,coord):
         --------------------------
         s -- array of multiple spin states, sorted in descending order
         spat_nums -- array of numbers of spatial states corresponding to the spin states
-        coord -- 0,1,2 for x,y,z
+        coord -- x,y,z
 
         Returns:
         --------------------------
-        S -- spin matrix in Productbasis of given spin and spatial states
+        Smat -- spin matrix in Productbasis of given spin and spatial states in Js
         """
-        multiplicities = []
-        for i in range(len(s)):
-                multiplicities.append(int(2*s[i]+1))
+        assert len(spin) == len(spat_nums)
+        multiplicities = [2*S+1 for S in spins]
         dim = int(np.sum(multiplicities*spat_nums))
-        S = np.zeros((dim,dim),dtype = complex)
+        Smat = np.zeros((dim,dim),dtype = complex)
         lower_bound = 0
         upper_bound = 0
-        for mult in multiplicities:
-                i = multiplicities.index(mult)
-                spinmat_x,spinmat_y,spinmat_z = get_spinmat(mult)
-                spinmat = [spinmat_x,spinmat_y,spinmat_z]
-                spinmat_prod = get_spinmat_prod(spinmat[coord],mult,spat_nums[i])
-                upper_bound += int(mult*spat_nums[i])
-                assert upper_bound-lower_bound == np.shape(spinmat_prod)[0]
-                assert upper_bound-lower_bound == np.shape(spinmat_prod)[1]
-                S[lower_bound:upper_bound,lower_bound:upper_bound] = spinmat_prod
-                lower_bound = upper_bound
-        return S
+        for i,mult in enumerate(multiplicities):
+            spinmat_coord = spin_utils.spinMat(mult,coord)
+            spinmat_prod = get_spinmat_prod(spinmat_coord,mult,spat_nums[i])
+            upper_bound += int(mult*spat_nums[i])
+            assert upper_bound-lower_bound == np.shape(spinmat_prod)[0]
+            assert upper_bound-lower_bound == np.shape(spinmat_prod)[1]
+            Smat[lower_bound:upper_bound,lower_bound:upper_bound] = spinmat_prod
+            lower_bound = upper_bound
+        return Smat
 
-def get_multipropmat_prod(l,s,spat_nums):
+def get_multipropmat_prod(l,spins,spat_nums):
         """
         Calculates the l matrix in the productbasis for multiple spins
 
         Args:
         --------------------------
-        l -- matrix of angular momentum that needs to be transformed in the basis for multiple spins
-        s -- array of multiple spin states, sorted in ascending order
+        l -- matrix of angular momentum that needs to be transformed in the basis for multiple spins in (kg*m^2)/s
+        s -- array of multiple spin states, sorted in ascending order 
         spat_nums -- array of numbers of spatial states corresponding to the spin states
 
         Returns:
         --------------------------
-        L -- matrix of angular momentum in Productbasis of given spin and spatial states
+        Lmat -- matrix of angular momentum in Productbasis of given spin and spatial states in (kg*m^2)/s
         """
-        multiplicities = []
-        for i in range(len(s)):
-                multiplicities.append(int(2*s[i]+1))
+        assert len(spins) == len(spat_nums)
+        multiplicities = [2*S+1 for S in spins]
         dim = int(np.sum(multiplicities*spat_nums))
-        L = np.zeros((dim,dim),dtype = complex)
+        Lmat = np.zeros((dim,dim),dtype = complex)
         lower_bound = 0
         upper_bound = 0
-        for mult in multiplicities:
-                i = multiplicities.index(mult)
-                propmat_prod = get_propmat_prod(l,mult,spat_nums[i])
-                upper_bound += int(mult*spat_nums[i])
-                assert upper_bound-lower_bound == np.shape(propmat_prod)[0]
-                assert upper_bound-lower_bound == np.shape(propmat_prod)[1]
-                L[lower_bound:upper_bound,lower_bound:upper_bound] = propmat_prod
-                lower_bound = upper_bound
-        return L
+        for i,mult in enumerate(multiplicities):
+            propmat_prod = get_propmat_prod(l,mult,spat_nums[i])
+            upper_bound += int(mult*spat_nums[i])
+            assert upper_bound-lower_bound == np.shape(propmat_prod)[0]
+            assert upper_bound-lower_bound == np.shape(propmat_prod)[1]
+            Lmat[lower_bound:upper_bound,lower_bound:upper_bound] = propmat_prod
+            lower_bound = upper_bound
+        return Lmat
 
 def transform_so(m,eigvecs):
         """
@@ -75,10 +125,10 @@ def transform_so(m,eigvecs):
 
         Returns:
         --------------------
-        M -- matrix m transformed into SO basis
+        Mmat -- matrix m transformed into SO basis
         """
-        M = np.matmul(np.conj(eigvecs.T),np.matmul(m,eigvecs))
-        return M
+        Mmat = np.matmul(np.conj(eigvecs.T),np.matmul(m,eigvecs))
+        return Mmat
 
 
 

packages/koehnlab/io/molpro_hdf5.py
@@ -1,9 +1,14 @@
 import numpy as np
 import h5py
+from enum import Enum
 from .basis_util import get_multispinmat_prod,get_multipropmat_prod,transform_so
 
+class Basis(Enum):
+    WF0 = "Zeroth order wavefunction"
+    PROD = "Spin-spatial Productbasis"
+    SO = "Spin-orbit coupled basis"
 
-def extract_data(path,prop,basis):
+def get_property_matrix(path:str,prop:str,basis: Basis):
     '''
     Returns the matrix of a given operator in a given basis which is stored
     in a HDF5 File in the given path.
@@ -13,8 +18,8 @@ def extract_data(path,prop,basis):
     path -- The path the HDF5 file is stored
     prop -- the operator of which the matrix is extracted from Molpro. 
             (DMX,DMY,DMZ,LX(),LY(),LZ(),SO)
-    basis -- the basis in which the property matrix should be returned.
-            (wf0,prod,so)
+    basis -- the basis in which the property matrix should be returned. 
+  	     Choose one of the three options of Basis Enum
     Returns:
     ---------------------
     PropMat -- The property matrix of given operator in the given basis
@@ -26,16 +31,17 @@ def extract_data(path,prop,basis):
         SpinStates = h5file['SpinStates'][:]
         SpatStates = h5file['SpatStates'][:]
     SO_2d = get_2dmat(SO)
-    if basis == 'wf0':
+    if basis == Basis.WF0:
         return PropMat
-    if basis == 'prod':
+    elif basis == Basis.PROD:
         if prop == 'so':
             PropMat = get_multispinmat_prod(PropMat,SpinStates,SpatStates)
         else:
             PropMat = get_multipropmat_prod(PropMat,SpinStates,SpatStates)
         return PropMat
-    if basis == 'so':
+    elif basis == Basis.SO:
         eigvecsh,eigvalsh = np.linalg.eigh(SO_2d)
         PropMat = transform_so(PropMat,eigvecsh)
         return PropMat
-
+    else: 
+        raise Exception("an error occurred","unexpected value for basis")

packages/koehnlab/spin_hamiltonians/g_tensor.py
@@ -2,65 +2,71 @@ import numpy as np
 from .phys_const import muBcm,g_e
 
 
-def get_mu(s,l):
+def get_magnetic_moment_matrix(s,l,unit: bool):
         """
         Computes the magnetic moment of the system in the basis of given spin and angular momentum matrix
-
+        Procedure taken from: L. F. Chibotaru, L. Ungur; Ab initio calculation of anisotropic magnetic properties of complexes. I. Unique definition of
+        pseudospin Hamiltonians and their derivation. J. Chem. Phys. 14 August 2012; 137 (6): 064112. https://doi.org/10.1063/1.4739763
         Args:
         ---------------------
-        s -- spin matrix
-        l -- angular momentum matrix
+        s -- spin matrix in Js
+        l -- angular momentum matrix in (kg*m^2)/s
+        unit -- Boolean if in multiples of bohr magneton
 
         Returns:
         ---------------------
-        mu -- matrix of magnetic moment
+        muMat -- matrix of magnetic moment in J/T
         """
-        mu = -muBcm*(g_e*s+l)
-        return mu
+        if unit:
+            muMat = -muBcm*(g_e*s+l)
+        else: 
+            muMat = -1*(g_e*s+l)
+        return muMat
 
-def get_A_matrix(mu_x,mu_y,mu_z,n):
+def get_A_matrix(mu_x,mu_y,mu_z,n:int):
         """
         Computes the A matrix to calculate g-Tensor afterwards
-
+        Procedure taken from: L. F. Chibotaru, L. Ungur; Ab initio calculation of anisotropic magnetic properties of complexes. I. Unique definition of
+        pseudospin Hamiltonians and their derivation. J. Chem. Phys. 14 August 2012; 137 (6): 064112. https://doi.org/10.1063/1.4739763
         Args:
         ---------------------
-        mu_x,y,z -- matrices of magnetic moment in SO basis in every dimension
+        mu_x,y,z -- matrices of magnetic moment in SO basis in every dimension in J/T
+        n -- number of states that are crucial, only needed if pseudospin is used so not all states are included in calculation
 
         Returns:
         ---------------------
-        A -- helper matrix for g-Tensor
+        Amat -- helper matrix for g-Tensor
         """
         mu = [mu_x[:n,:n],mu_y[:n,:n],mu_z[:n,:n]]
         n = len(mu)
-        A = np.zeros((n,n))
+        Amat = np.zeros((n,n))
         for alpha in range(n):
                 for beta in range(n):
                         prod = np.matmul(mu[alpha],mu[beta])
                         trace = np.matrix.trace(prod)
                         assert np.abs(trace.imag) < 1E-6
-                        A[alpha,beta] = 0.5*trace.real
-        return A
+                        Amat[alpha,beta] = 0.5*trace.real
+        return Amat
 
-def get_g_tensor(A,multiplicity):
+def get_g_tensor(A,multiplicity:int):
         """
-        returns the g-Tensorin cartesian coordinates and his main values  of given helpermatrix A and given multiplicity of the system, also returns the ro$        Args:
+        returns the g-Tensorin cartesian coordinates and his main values  of given helpermatrix A and given multiplicity of the system,
+        also returns the rotation matrix between main axes and main magnetic axes. 
+        Procedure taken from: L. F. Chibotaru, L. Ungur; Ab initio calculation of anisotropic magnetic properties of complexes. I. Unique definition of
+        pseudospin Hamiltonians and their derivation. J. Chem. Phys. 14 August 2012; 137 (6): 064112. https://doi.org/10.1063/1.4739763
+        Args:
         ----------------------
         A -- helper matrix
         multiplicity -- multiplicity of the system
+
         Returns:
         ----------------------
-        g -- g-Tensor
-        g_eig -- main value of g-Tensor
-        R -- rotation matrix
+        g_diag -- eigenvalues of g-Tensor on diagonal
+        Rmat -- rotation matrix
         """
-        mu_B = 1
         S = 0.5*(multiplicity-1)
         g_diag = np.zeros(np.shape(A))
-        eigvalA,R = np.linalg.eigh(A)
+        eigvalA,Rmat = np.linalg.eigh(A)
         for i in range(len(eigvalA)):
                 g_diag[i,i] =np.sqrt(6*eigvalA[i]/(S*(S+1)*(2*S+1)))
-        g = np.dot(np.dot(R,g_diag),np.linalg.inv(R))
-        g_eig = np.diag(g_diag)
-        if np.linalg.det(R) == -1:
-                R = -1*R                                                                                                                                           
-        return g,g_eig,R
+        return g_diag,Rmat